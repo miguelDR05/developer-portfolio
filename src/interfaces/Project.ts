4c76f7a444c77f29c370c51559f5d752d7f691cd
miguelDR05 <miguel05.dev@gmail.com>
2025-10-19 15:19:37 -0500

    feat: Add About, Contact, Hero, Projects sections and Footer component
    - Implemented About section with personal information and skills.
    - Created Contact section with a static form for inquiries.
    - Developed Hero section to introduce the portfolio.
    - Added Projects section to showcase featured projects with descriptions and links.
    - Established Footer component with social media links and copyright information.
    - Included global styles for layout and responsiveness.
    - Created Project interface and JSON data for project details.

---
+++ src/interfaces/Project.ts
@@ -0,0 +1,11 @@
+// crear una interfaz para Project
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  link: string;
+  stack: string[];
+  github: string;
+  demo: string;
+  image: string;
+}
